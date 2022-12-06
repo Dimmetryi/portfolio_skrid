95f5dac81e2ffa0af48d3793161e82e87b5d7eb8
Dimmetryi <dima.gurbo.2003@mail.ru>
2022-12-06 22:06:15 +0300

    new kommit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    // $('.header_burger').click(function(event){
-    //     $('.header_burger .nav_menu').toggleClass('active');
-    // });
-    console.log(123);
+    $('.header_burger').click(function(event){
+        $('.header_burger, .nav_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
